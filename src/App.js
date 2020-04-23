339bc81a1901fa607d6edae915cbb4a89a014e32
JM <manzur-1990@hotmail.com>
2020-04-22 23:14:34 -0500

    MVP met

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserForm from './components/Form'
+
+
+function App() {
+  return (
+    <div className="container">
+
+      <UserForm />
+
+    </div>
+  );
+}
+
+export default App;
